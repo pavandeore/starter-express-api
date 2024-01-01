00a3077465386fdd55e6efc1c508846820bdc78a
pawan <pavandeore90@gmail.com>
2024-01-01 16:53:18 +0530

    functional gemini-pro working

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run(LANGUAGE, DATA) {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = "Give me a "+LANGUAGE+" REST API WRAPPER code for below Functions \n" + DATA;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return text;
+}
+
+app.use(express.json());
+
+app.post("/text", (req, res) => {
+    const { language, text } = req.body;
+
+    if (!text) {
+        return res.status(400).json({ error: "No text provided" });
+    }
+
+    run(language, text).then((data) => {
+        return res.status(200).json({ data });
+    });
+});
+
+
+app.listen(process.env.PORT || 3000);
