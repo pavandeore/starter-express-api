72caff88bc8f149f9b7f2c20fd089e976a3b5692
pawan <pavandeore90@gmail.com>
2024-01-20 17:11:48 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run(LANGUAGE, DATA) {
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const prompt = "Give me a "+LANGUAGE+" REST API WRAPPER code for below Functions \n" + DATA;
+    const prompt = "Give me a "+LANGUAGE+" REST API WRAPPER code ONLY (no explanation) for below Functions \n" + DATA;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
@@ -15,6 +16,7 @@ async function run(LANGUAGE, DATA) {
 }
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/text", (req, res) => {
     const { language, text } = req.body;
